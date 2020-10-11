bb311c4b424f24dc2b4c8b621cd4c13966bc6d74
Ömer Doğan <omer.dogan@kariyer.net>
2020-10-12 00:57:25 +0300

    project created, client js edited, vue installed

---
+++ src/views/Dashboard.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="container">
+    <LineChart :chartdata="chartdata"
+               :options="options" />
+  </div>
+</template>
+
+<script>
+import LineChart from '../components/Chart'
+
+export default {
+  name: 'LineChartContainer',
+  components: { LineChart },
+  data: () => ({
+    loaded: false,
+    chartdata: null
+  }),
+  async mounted() {
+    this.loaded = false
+    try {
+      const { userlist } = await fetch('/api/userlist')
+      this.chartdata = userlist
+      this.loaded = true
+    } catch (e) {
+      console.error(e)
+    }
+  }
+}
+</script>
